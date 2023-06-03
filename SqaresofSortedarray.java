af1b895de6413397ce64df212797d2657607eef2
pratyushdev-codes <birole.pratyush@gmail.com>
2023-06-03 15:34:59 +0530

    Sqare of an array

---
+++ SqaresofSortedarray.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class SqaresofSortedarray {
+    public static void main(String[] args) {
+        int[] nums = {-11, 2, 4, 6, 8, 10, 11};
+        int[] result = sortedSquares(nums);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] sortedSquares(int[] arr) {
+        if (arr.length == 0) {
+            return new int[]{-1, -1};
+        } else {
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = arr[i] * arr[i];
+            }
+            Arrays.sort(arr);  // Sort the array in non-decreasing order
+            return arr;
+        }
+    }
+}
